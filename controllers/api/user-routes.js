5d663d0bd2643b50e25459e8ebeb5ff074c438ae
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-17 09:35:16 -0500

    user-routes CRUD setup created

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET route for all users html_ex: /api/users
+router.get('/', (req, res) => {
+
+});
+
+// GET route for specific users by ID html_ex: /api/users/2
+router.get('/:id', (req, res) => {
+
+});
+
+// POST route for adding users to the web html_ex: /api/users
+router.post('/', (req, res) => {
+
+});
+
+// PUT route for updating specific users by ID html_ex: /api/users/3
+router.put('/:id', (req, res) => {
+
+});
+
+// DELETE route for removing specific users by ID html_ex: /api/users/4
+router.delete('/:id', (req, res) => {
+    
+});
+
+module.exports = router;
