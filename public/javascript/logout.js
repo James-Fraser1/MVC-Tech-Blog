af062ad2ea1c15a93eb94d4d39ce98d594f6bc80
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-17 10:30:36 -0500

    logout.js functionality created with onclick feature redirecting user back to login page

---
+++ public/javascript/logout.js
@@ -0,0 +1,20 @@
+const { error } = require("console");
+
+async function logoutPage() {
+    console.log("Logout Page Reached!")
+    const logoutFeedback = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (logoutFeedback.ok) {
+        document.location.replace('/');
+    } else {
+        console.error(error);
+    }
+}
+
+// OnClick Button navigates logs out user returning them to login page
+document.getElementById('logout-form').onclick(logoutPage);
