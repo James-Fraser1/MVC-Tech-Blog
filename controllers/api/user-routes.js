ce585b400399494eb3f0215eb533f96108cc1541
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-17 10:23:04 -0500

    routing updated, model structure edited, variables checked and tested for server functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,7 +13,7 @@ router.get('/:id', (req, res) => {
 
 // POST route for adding users to the web html_ex: /api/users
 router.post('/', (req, res) => {
-
+    
 });
 
 // PUT route for updating specific users by ID html_ex: /api/users/3
@@ -23,7 +23,7 @@ router.put('/:id', (req, res) => {
 
 // DELETE route for removing specific users by ID html_ex: /api/users/4
 router.delete('/:id', (req, res) => {
-    
+
 });
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/loginconnection');
+const sequelize = require('../config/connection');
 const saltRounds = 10;
 
 class User extends Model {
