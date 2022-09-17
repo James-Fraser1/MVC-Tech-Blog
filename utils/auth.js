e3dfb02505d29e48cc94de9d207a8328de806f66
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-17 10:32:10 -0500

    auth page created with routing functionality

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+
+//   Auth Page makes sure login functionality is up and running, otherwise user cannot progress to dashboard page
