0199a9725b700d7218dc05039d7390e06befb617
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-17 09:03:59 -0500

    updated routing and file structures. testing routes and connectivity to port

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-// create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: '127.0.0.1',
-  dialect: 'mysql',
-  port: 3306
-});
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize('techblog_db', 'root', 'gyno-4128', {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
