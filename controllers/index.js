8c537cb11a67781ed08524fd259306d693b24acf
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-28 19:41:04 -0400

    dashboard reedited, homepage-routes adjusted as well as index.js for functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homepage-route');
+const homeRoutes = require('./homepage-routes');
+// const dashboardRoutes = require('./dashboard');
 
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+// router.use('/dashboard', dashboardRoutes);
+
 
 module.exports = router;
