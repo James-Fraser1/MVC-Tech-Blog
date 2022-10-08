9d964a2aa2c242144e2e9250c06f3e53722491d8
James-Fraser1 <jdfraser2022@gmail.com>
2022-10-08 11:36:50 -0400

    new changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-<<<<<<< HEAD
-const homeRoutes = require('./homepage-route');
-const dashboardRoutes = require('./dashboard-routes');
-=======
 const homeRoutes = require('./homepage-routes');
-// const dashboardRoutes = require('./dashboard');
->>>>>>> 6dd3117b391204df1e339a6c70bd3ef51d29fd20
+const dashboardRoutes = require('./dashboard');
 
 router.use('/', homeRoutes);
-<<<<<<< HEAD
-router.use('/dashboard', dashboardRoutes)
-=======
 router.use('/api', apiRoutes);
-// router.use('/dashboard', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 
->>>>>>> 6dd3117b391204df1e339a6c70bd3ef51d29fd20
 
 module.exports = router;
